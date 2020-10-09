83315a27d44fccbab60c768784a6e4ffaaa67acd
konstrictor <jonkur96@gmail.com>
2020-10-09 15:40:05 +0200

    initial commit: login actity UI

---
+++ app/src/main/java/bi/konstrictor/aacbflights/LoginActivity.java
@@ -0,0 +1,18 @@
+package bi.konstrictor.aacbflights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void performLogin(View view) {
+    }
+}
